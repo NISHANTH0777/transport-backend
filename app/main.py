d78a0485c0f4869c1ca88765e20e1f574e3a6171
Nishanth s <nn1884514@gmail.com>
2026-01-03 10:30:14 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="SPUF-314 BMTC Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",]
+    allow_origins=["http://localhost:5173","https://spuf-314-frontend.vercel.app"]
     ,
     allow_credentials=True,
     allow_methods=["*"],
